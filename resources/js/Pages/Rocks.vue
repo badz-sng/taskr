d6ed37a462c9e8c100c1cf7bc2bd3dce63281648
Emmanuel Sunga <emmanuelsunga325@gmail.com>
2024-11-02 01:06:37 +0800

    removed extra file

--- resources/js/Pages/Rocks.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div>
-        <h1>BATO!!!!!</h1>
-        <router-link to="/test"> Take me to Test page </router-link>
-        <router-link to="/"> Take me to Home page </router-link>
-    </div>
-</template>
